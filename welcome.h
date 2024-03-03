7010b42cd210b2bb1d2446df4fb8ba80274f7415
Sama3mad <159120293+Sama3mad@users.noreply.github.com>
2024-03-03 14:52:30 +0200

    Update welcome.h

--- welcome.h
+++ welcome.h
@@ -1,1 +1,27 @@
+#ifndef WELCOME_H
+#define WELCOME_H
+
+#include <QDialog>
+
+namespace Ui {
+class Welcome;
+}
+
+class Welcome : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Welcome(QWidget *parent = nullptr);
+    void setnameandage(QString, int);
+    ~Welcome();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Welcome *ui;
+};
+
+#endif // WELCOME_H
 
