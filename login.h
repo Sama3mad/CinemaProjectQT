02f3cbaff661baa322eadb9a21fdb329c340a72e
Sama3mad <159120293+Sama3mad@users.noreply.github.com>
2024-03-03 14:52:04 +0200

    Update login.h

--- login.h
+++ login.h
@@ -1,1 +1,28 @@
-//hi iam sama
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Login;
+}
+QT_END_NAMESPACE
+
+class Login : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Login(QWidget *parent = nullptr);
+    ~Login();
+
+private slots:
+    void on_pushButtonLogin_clicked();
+
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::Login *ui;
+};
+#endif // LOGIN_H
